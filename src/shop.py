9ba97e1e4c6cbe6f522251966755a1639d4a91e6
Davidek523 <davidov520@wp.pl>
2025-03-17 12:33:29 +0100

    Added basestate
    Added:
    - base state and the methods to the shop class

--- src/shop.py
+++ src/shop.py
@@ -1,9 +1,9 @@
 import pygame
 
 from src.settings import WORLD_LAYERS
-# from src.states.base_state import BaseState
+from src.states.base_state import BaseState
 
-class ShowShop(pygame.sprite.Sprite):
+class ShowShop(pygame.sprite.Sprite, BaseState):
     def __init__(self, pos, surface, groups, z = WORLD_LAYERS["main"]):
         super().__init__(groups)
 
@@ -11,3 +11,9 @@ class ShowShop(pygame.sprite.Sprite):
         self.image.fill("white")
         self.rect = self.image.get_frect(topleft = pos)
         self.z = z
+
+    def update(self, *args, **kwargs):
+        return super().update(*args, **kwargs)
+    
+    def render(self, screen):
+        return super().render(screen)
