8c79550a9662b2981c2ac0a8681fff699d68edb1
Davidek523 <davidov520@wp.pl>
2025-03-21 00:37:39 +0100

    Drawin text elsewhere
    Changed:
    - position of the text to top right
    - setup to use the json data

--- src/states/player_gui.py
+++ src/states/player_gui.py
@@ -10,10 +10,17 @@ class Player_gui():
     def __init__(self, screen):
         self.font = pygame.font.Font(None, 36)
         self.screen = screen
+        self.margin = 20
+        self.wallet = {}
 
     def draw_gui(self, screen: pygame.Surface):
-        player_money = self.font.render("Gold: N/A", True, (200, 200, 200))
-        self.screen.blit(player_money, (50, screen.get_height() - 60))
+        for item, quantity in self.wallet:
+            player_money = self.font.render(f"Gold: {quantity}", True, (255, 255, 255))
+
+        text_wdith, text_height = player_money.get_size()
+        x_pos = screen.get_width() - text_wdith - self.margin
+        y_pos = self.margin
+        self.screen.blit(player_money, (x_pos, y_pos))
 
         screen.blit(self.screen, dest=(0, 0))
         pygame.display.flip()  # Update the display
