83fd970c12c95fd1c8a4193538f6c114b119600c
Davidek523 <davidov520@wp.pl>
2025-03-21 00:21:01 +0100

    Drawing gui
    Added:
    - drew the gui onto the screen

--- src/states/player_gui.py
+++ src/states/player_gui.py
@@ -3,21 +3,23 @@ from typing import Dict, Tuple
 import pygame
 
 from src.states.base_state import BaseState
-from settings import SCREEN_WIDTH, SCREEN_HEIGHT
+from src.settings import SCREEN_WIDTH, SCREEN_HEIGHT
 
-class Player_gui(BaseState):
-    def __init__(self, game_state_manager):
-        super().__init__(game_state_manager)
+class Player_gui():
+    def __init__(self, screen):
         self.font = pygame.font.Font(None, 36)
-        self.screen = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = screen
 
-    def udpate(self, events):
-        for event in events:
-            match event.type:
-                case pygame.KEYDOWN:
-                    if pygame.K_o:
-                        self.game_state_manager.exit_state()
+    # def update(self, events):
+    #     for event in events:
+    #         match event.type:
+    #             case pygame.KEYDOWN:
+    #                 if pygame.K_o:
+    #                     self.game_state_manager.exit_state()
 
-    def render(self, screen: pygame.Surface):
+    def draw_gui(self, screen: pygame.Surface):
         player_money = self.font.render("Gold: N/A", True, (200, 200, 200))
-        screen.blit(player_money, (50, self.screen.get_height() - 60))
+        self.screen.blit(player_money, (50, screen.get_height() - 60))
+
+        screen.blit(self.screen, dest=(0, 0))
+        pygame.display.flip()  # Update the display
