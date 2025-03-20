2d28d2ea2db60970d0d8d656b0b9a53751296c01
Davidek523 <davidov520@wp.pl>
2025-03-20 19:31:37 +0100

    Putting logic behind player_gui
    Added:
    - basic logic to the player gui

--- src/states/player_gui.py
+++ src/states/player_gui.py
@@ -3,8 +3,21 @@ from typing import Dict, Tuple
 import pygame
 
 from src.states.base_state import BaseState
+from settings import SCREEN_WIDTH, SCREEN_HEIGHT
 
 class Player_gui(BaseState):
     def __init__(self, game_state_manager):
         super().__init__(game_state_manager)
-        
+        self.font = pygame.font.Font(None, 36)
+        self.screen = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    def udpate(self, events):
+        for event in events:
+            match event.type:
+                case pygame.KEYDOWN:
+                    if pygame.K_o:
+                        self.game_state_manager.exit_state()
+
+    def render(self, screen: pygame.Surface):
+        player_money = self.font.render("Gold: N/A", True, (200, 200, 200))
+        screen.blit(player_money, (50, self.screen.get_height() - 60))
