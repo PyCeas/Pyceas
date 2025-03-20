20480feb33cbbd64f7c00f00fde3d5d775030bd2
Davidek523 <davidov520@wp.pl>
2025-03-21 00:23:20 +0100

    Removed code
    Removed:
    - unused code

--- src/states/player_gui.py
+++ src/states/player_gui.py
@@ -5,17 +5,11 @@ import pygame
 from src.states.base_state import BaseState
 from src.settings import SCREEN_WIDTH, SCREEN_HEIGHT
 
+
 class Player_gui():
     def __init__(self, screen):
         self.font = pygame.font.Font(None, 36)
         self.screen = screen
-
-    # def update(self, events):
-    #     for event in events:
-    #         match event.type:
-    #             case pygame.KEYDOWN:
-    #                 if pygame.K_o:
-    #                     self.game_state_manager.exit_state()
 
     def draw_gui(self, screen: pygame.Surface):
         player_money = self.font.render("Gold: N/A", True, (200, 200, 200))
